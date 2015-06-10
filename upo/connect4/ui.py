d3b7b76201a3e78b457c158d7bc22b70e9ddba8e
sguazt <marco.guazzone@gmail.com>
2015-06-10 17:41:56 +0200

    Added a countdown timer (but actually it is not used since it does not keep track of the time spent inside non graphical functions). Also fixed blinking of winning tokens in order to work with differnt frame rate

--- upo/connect4/ui.py
+++ upo/connect4/ui.py
@@ -42,14 +42,18 @@ class PyGameUI(GameUI):
     #DEFAULT_FG_COLOR = (  0,   0,   0)
     #DEFAULT_BG_COLOR = (255, 255, 255)
     DEFAULT_FPS = 30
-    DEFAULT_GEOMETRY = (640, 480) # window geometry (width, height)
+    DEFAULT_GEOMETRY = (800, 600) # window geometry (width, height)
     DEFAULT_LAYOUT = (7, 6) # board (width, height)
+    DEFAULT_TIMEOUT = 0 # maximum number of second before a timeout is triggered for an agent
 
-    def __init__(self, game, geometry = DEFAULT_GEOMETRY, fps = DEFAULT_FPS):
+    def __init__(self, game, geometry = DEFAULT_GEOMETRY, fps = DEFAULT_FPS, timeout = DEFAULT_TIMEOUT):
         GameUI.__init__(self, game)
         self.win_geometry = geometry
         self.fps = fps
+        self.timeout = timeout
         self.display = None
+        self.timer_font = None
+        self.legend_font = None
         self.bg_color = (255, 255, 255) # white
         self.fg_color = (128, 128, 128) # gray
         self.column_highlight_color = (0, 255, 0) # green
@@ -61,18 +65,18 @@ class PyGameUI(GameUI):
         else:
             # Use standard colors: red and yellow
             self.player_colors = [(255, 0, 0), (255, 255, 0)]
-        #self.tile_geom = (int(geometry[0]/layout[0]), int(geometry[1]/layout[1]))
-        #self.yoffs = int((self.win_geometry[0]-layout[0]*self.tile_geom[0])/2)
-        #self.xoffs = int((self.win_geometry[1]-layout[1]*self.tile_geom[1])/2)
+        #self.tile_geom = (geometry[0]//layout[0], geometry[1]//layout[1])
+        #self.yoffs = (self.win_geometry[0]-layout[0]*self.tile_geom[0])//2
+        #self.xoffs = (self.win_geometry[1]-layout[1]*self.tile_geom[1])//2
         board_cols = self.game.get_state().get_board().width()
         board_rows = self.game.get_state().get_board().height()
-        self.tile_size = int(min(self.win_geometry)/max(board_cols, board_rows))
+        self.tile_size = min(self.win_geometry)//max(board_cols, board_rows)
         self.board_dim = (self.tile_size*board_cols, self.tile_size*board_rows)
-        self.xoffs = int((self.get_window_width()-self.board_dim[0])/2)
-        self.yoffs = int((self.get_window_height()-self.board_dim[1])/2)
+        self.xoffs = (self.get_window_width()-self.board_dim[0])//2
+        self.yoffs = (self.get_window_height()-self.board_dim[1])//2
         #self.board_rect = pygame.Rect(self.xoffs, self.yoffs+self.tile_size, self.board_dim, self.board_dim)
         self.board_rect = pygame.Rect(self.xoffs, self.yoffs, self.board_dim[0], self.board_dim[1])
-        self.token_radius = int(self.tile_size/2) - 1
+        self.token_radius = self.tile_size//2 - 1
         #print('Window: ', self.win_geometry, ', Board: ', self.board_dim, ', Tile: ', self.tile_size, ', Offset: ', (self.xoffs, self.yoffs))
 
     def get_geometry(self):
@@ -95,91 +99,110 @@ class PyGameUI(GameUI):
         self.display = pygame.display.set_mode(self.win_geometry)
         pygame.display.set_caption('Connect 4')
 
-        #self.red_pile_rect = pygame.Rect(int(SPACE_SIZE/2), self.size[1]-int(3*SPACE_SIZE/2), SPACE_SIZE, SPACE_SIZE)
+        # Preloads font to avoid delays during the execution
+        self.legend_font = pygame.font.SysFont(pygame.font.get_default_font(), 16)
+        if self.timeout > 0:
+            self.timer_font = pygame.font.Font('resources/timer_font.ttf', 25)
 
-        #cur_agent = 0
         winning_cells = []
         game_over = False
-        winning_highlight = True
-        winning_highlight_count = 0
-        WIN_HIGHLIGHT_MAX_COUNT = 10
+        winning_blink = True
+        winning_blink_count = 0
+        WIN_BLINK_DURATION = 0.5 # number of seconds elasping between 2 successive blinking
         column_highlight = False
         column_highlight_pos = ()
+        timer_count = 0
+        timer_value = 0
+        skip_logic = True
         while True:
+            # This is the game loop
+
+            if skip_logic:
+                skip_logic = False
+            else:
+                wait_interaction = not game_over and self.game.get_current_agent().is_interactive()
+
+                # Checks for interesting events in the event loop
+                for event in pygame.event.get():
+                    #print('Event: ', event)
+                    # Checks for a quit event
+                    if (event.type == pygame.QUIT
+                        or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
+                        pygame.quit()
+                        return
+                    # In case of interactive agent, there are more interesting things to check
+                    if wait_interaction:
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                            # Pushes the token on the corresponding board column
+                            if self.is_inside_board(event.pos):
+                                (c, r) = self.screen_to_board_coords(event.pos)
+                                if self.game.get_state().is_legal_action(c):
+                                    self.game.get_current_agent().set_action(c)
+                                    wait_interaction = False
+                        else:
+                            wait_interaction = True
+                            # Draws a highlight box around the column under the mouse pointer
+                            if event.type == pygame.MOUSEMOTION and self.is_inside_board(event.pos):
+                                column_highlight = True
+                                column_highlight_pos = event.pos
+                            else:
+                                column_highlight = False
+                # Plays the game
+                if not wait_interaction:
+                    if not game_over:
+                        self.game.make_move()
+                        game_over = self.game.is_over()
+                        if game_over:
+                            if self.game.get_state().is_win():
+                                winning_cells = self.game.get_state().get_winner_positions()
+                                winning_agent = self.game.get_agent(self.game.get_state().get_winner())
+                                print("The winner is: " + winning_agent.get_name() + "!")
+                            elif self.game.get_state().is_tie():
+                                print("Tie!")
+                        if self.timeout > 0:
+                            timer_count = 0
+                # Updates the timer
+                if self.timeout > 0 and not game_over:
+                    # Computes total seconds
+                    timer_value = self.timeout - timer_count//self.fps
+                    if timer_value < 0:
+                        timer_value = 0
+                    if timer_value == 0:
+                        game_over = True
+                        print("Timed out! " + self.game.get_current_agent().get_name() + " loses!")
+                    timer_count += 1
+ 
+            ## BEGIN DRAWING
+
+            # Fills the display with the background color
             self.display.fill(self.bg_color)
 
-            wait_interaction = not game_over and self.game.get_current_agent().is_interactive()
-
-            # Checks for interesting events
-            for event in pygame.event.get():
-                #print('Event: ', event)
-                # Checks for a quit event
-                if (event.type == pygame.QUIT
-                    or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
-                    pygame.quit()
-                    return
-                # In case of interactive agent, there are more interesting things to check
-                if wait_interaction:
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        # Pushes the token on the corresponding board column
-                        if self.is_inside_board(event.pos):
-                            (c, r) = self.screen_to_board_coords(event.pos)
-                            if self.game.get_state().is_legal_action(c):
-                                self.game.get_current_agent().set_action(c)
-                                wait_interaction = False
-                    else:
-                        wait_interaction = True
-                        # Draws a highlight box around the column under the mouse pointer
-                        if event.type == pygame.MOUSEMOTION and self.is_inside_board(event.pos):
-                            column_highlight = True
-                            column_highlight_pos = event.pos
-                        else:
-                            column_highlight = False
-
-            #if not wait_interaction:
-            #    if game_over or self.game.is_over():
-            #        if not game_over:
-            #            game_over = True
-            #            if self.game.get_state().is_win():
-            #                print("Winning!")
-            #                winning_cells = self.game.get_state().get_winner_positions()
-            #            elif self.game.get_state().is_tie():
-            #                print("Tie!")
-            #        #elif winning_cells.empty():
-            #        #    winning_cells = self.game.get_winner_cells()
-            #    else:
-            #        self.game.make_move()
-            #    #elif not self.game.get_current_agent().is_interactive():
-            #    #    self.game.make_move()
-            if not wait_interaction:
-                if not game_over:
-                    self.game.make_move()
-                    game_over = self.game.is_over()
-                    if game_over:
-                        if self.game.get_state().is_win():
-                            winning_cells = self.game.get_state().get_winner_positions()
-                            winning_agent = self.game.get_agent(self.game.get_state().get_winner())
-                            print("The winner is: " + winning_agent.get_name() + "!")
-                        elif self.game.get_state().is_tie():
-                            print("Tie!")
-
+            # Draws the game board
             self.draw_board()
 
+            # Draws agents' legend
             self.draw_legend()
 
+            # Highlights the board column under the mouse
             if not game_over and column_highlight:
                 self.highlight_column(column_highlight_pos)
 
+            # Blinks winning tokens
             if len(winning_cells) > 0:
-                # Highlights winning tokens
-                if winning_highlight_count == WIN_HIGHLIGHT_MAX_COUNT:
-                    winning_highlight = not winning_highlight
-                    winning_highlight_count = 0
-                if winning_highlight:
-                    self.highlight_winning_tokens(winning_cells)
-                winning_highlight_count += 1
+                if winning_blink_count/self.fps >= WIN_BLINK_DURATION:
+                    winning_blink = not winning_blink
+                    winning_blink_count = 0
+                if winning_blink:
+                    self.blink_winning_tokens(winning_cells)
+                winning_blink_count += 1
+
+            # Displays a countdown timer
+            if self.timeout > 0:
+                self.draw_timer(timer_value)
 
             pygame.display.flip();
+
+            ## END DRAWING
 
             fps_clock.tick(self.fps)
 
@@ -222,8 +245,8 @@ class PyGameUI(GameUI):
         game_board = self.game.get_state().get_board()
         if game_board.has_token(column, row):
             token = game_board.get_token(column, row)
-            xc = self.board_rect.left + column*self.tile_size + int(self.tile_size/2) + 1
-            yc = self.board_rect.top + row*self.tile_size + int(self.tile_size/2) + 1
+            xc = self.board_rect.left + column*self.tile_size + self.tile_size//2 + 1
+            yc = self.board_rect.top + row*self.tile_size + self.tile_size//2 + 1
             pygame.draw.circle(self.display, self.player_colors[token], (xc, yc), self.token_radius)
 
     def highlight_column(self, pos):
@@ -234,7 +257,7 @@ class PyGameUI(GameUI):
         (x, y) = self.board_to_screen_coords((c, r))
         pygame.draw.rect(self.display, self.column_highlight_color, (x-2, self.board_rect.top-2, self.tile_size+2, self.board_rect.height+2), 2)
 
-    def highlight_winning_tokens(self, winning_cells):
+    def blink_winning_tokens(self, winning_cells):
         """
         Highlights winning tokens.
         """
@@ -247,15 +270,14 @@ class PyGameUI(GameUI):
                 #hcol[0] = 255-col[0]
                 #hcol[1] = 255-col[1]
                 #hcol[2] = 255-col[2]
-                xc = self.board_rect.left + x*self.tile_size + int(self.tile_size/2) + 1
-                yc = self.board_rect.top + y*self.tile_size + int(self.tile_size/2) + 1
+                xc = self.board_rect.left + x*self.tile_size + self.tile_size//2 + 1
+                yc = self.board_rect.top + y*self.tile_size + self.tile_size//2 + 1
                 pygame.draw.circle(self.display, hcol, (xc, yc), self.token_radius, 2)
 
     def draw_legend(self):
         """
         Draws the agents' legend.
         """
-        font = pygame.font.SysFont(pygame.font.get_default_font(), 16)
         #font_col = (255-x for x in self.bg_color)
         font_col = (255-self.bg_color[0],255-self.bg_color[1],255-self.bg_color[2])
         radius = int(self.token_radius*0.20)
@@ -264,16 +286,26 @@ class PyGameUI(GameUI):
             xc = self.board_rect.right + 10
             yc = self.board_rect.top + radius + idx*radius*4
             pygame.draw.circle(self.display, self.player_colors[idx], (xc, yc), radius)
-            font_surf = font.render(agent.get_name(), True, font_col)
+            font_surf = self.legend_font.render(agent.get_name(), True, font_col)
             self.display.blit(font_surf, (xc+6, yc-6))
+
+    def draw_timer(self, timer_value):
+        # Gets total minutes and seconds
+        (mins, secs) = divmod(timer_value, 60)
+        font = pygame.font.SysFont(pygame.font.get_default_font(), 25)
+        timer_str = "{0:02}:{1:02}".format(mins, secs)
+        font_col = (255-self.bg_color[0],255-self.bg_color[1],255-self.bg_color[2])
+        font_timer = self.timer_font.render(timer_str, True, font_col)
+        #rect = font_timer.get_rect()
+        self.display.blit(font_timer, (self.board_rect.left, self.board_rect.bottom+(self.win_geometry[1]-self.board_rect.bottom)//2))
 
     def board_to_screen_coords(self, pos):
         """
         Transforms board position (column, row) into screen coordinates (x, y).
         """
         (c, r) = pos
-        x = int(c*self.tile_size+self.board_rect.left)
-        y = int(r*self.tile_size+self.board_rect.top)
+        x = c*self.tile_size+self.board_rect.left
+        y = r*self.tile_size+self.board_rect.top
         return (x, y)
 
     def screen_to_board_coords(self, pos):
@@ -281,8 +313,8 @@ class PyGameUI(GameUI):
         Transforms screen position (x, y) into board coordinates (column, row).
         """
         (x, y) = pos
-        c = int((x-self.board_rect.left)/self.tile_size)
-        r = int((y-self.board_rect.top)/self.tile_size)
+        c = (x-self.board_rect.left)//self.tile_size
+        r = (y-self.board_rect.top)//self.tile_size
         return (c, r)
 
     def is_inside_board(self, pos):
